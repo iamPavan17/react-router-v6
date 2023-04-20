900641c37e71d8bbcf7fae6303d946c3de20b0a3
Pavan S <iampavan05@gmail.com>
2023-04-20 11:51:34 +0530

    ✨ init setup > react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
@@ -7,6 +8,11 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* </BrowserRouter> is going to provide a bunch of things related to routing to all the components in our application. 
+        Uses HTML5 history API (push/replace/pop - state).
+    */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
