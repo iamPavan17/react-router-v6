6c98389083f3c74ced60bf73b298eb073d5280f5
Pavan S <iampavan05@gmail.com>
2023-05-04 12:10:25 +0530

    ✨ useSearchParams

--- src/pages/Profile/ViewProfile.js
+++ src/pages/Profile/ViewProfile.js
@@ -1,7 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+
 export default function ViewProfile() {
+  //  The useSearchParams hook is used to read and modify the query string in the URL for the current location.
+  //   Returns an array.
+  const [searchParams, setSearchParams] = useSearchParams();
+  const userId = searchParams.get("user"); // example: http://localhost:3000/profile?user=3
+
   return (
     <div>
       <h1>ViewProfile</h1>
+      <p>User ID - {userId}</p>
+      <button
+        onClick={() =>
+          setSearchParams({ user: Math.floor(Math.random() * 10) + 1 })
+        }
+      >
+        Update user param
+      </button>
     </div>
   );
 }
